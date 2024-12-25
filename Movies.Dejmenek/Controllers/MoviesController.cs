7f62ae0f2c18142f9e50ed03e80dc716e9889a2a
Dejmenek <dejmen14567@gmail.com>
2024-12-25 22:16:18 +0100

    feat: add MoviesController and CRUD views for movies
    Added MoviesController.cs to handle CRUD operations for movies, including listing with filtering, sorting, and pagination. Added Create.cshtml for movie creation, Delete.cshtml for deletion confirmation, Details.cshtml for displaying movie details, Edit.cshtml for editing movies, and Index.cshtml for listing movies with filtering, sorting, and pagination.

---
+++ Movies.Dejmenek/Controllers/MoviesController.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Movies.Dejmenek.Data;
+using Movies.Dejmenek.Enums;
+using Movies.Dejmenek.Helpers;
+using Movies.Dejmenek.Models;
+
+namespace Movies.Dejmenek.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MovieContext _context;
+
+        public MoviesController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index(
+            string movieGenre,
+            string movieRating,
+            int sortOption,
+            string searchString,
+            int? pageNumber
+        )
+        {
+            if (_context.Movie == null) return Problem("Entity set 'MvcMovieContext.Movie' is null.");
+
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            IQueryable<string> ratingsQuery = from m in _context.Movie
+                                              orderby m.Rating
+                                              select m.Rating;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                pageNumber = 1;
+                movies = movies.Where(m => m.Title!.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(m => m.Genre == movieGenre);
+            }
+
+            if (!string.IsNullOrEmpty(movieRating))
+            {
+                movies = movies.Where(m => m.Rating == movieRating);
+            }
+
+            movies = (SortOptions)sortOption switch
+            {
+                SortOptions.TitleDesc => movies.OrderByDescending(m => m.Title),
+                SortOptions.DateDesc => movies.OrderByDescending(m => m.ReleaseDate),
+                SortOptions.DateAsc => movies.OrderBy(m => m.ReleaseDate),
+                SortOptions.PriceDesc => movies.OrderByDescending(m => m.Price),
+                SortOptions.PriceAsc => movies.OrderBy(m => m.Price),
+                _ => movies.OrderBy(m => m.Title),
+            };
+
+            int pageSize = 3;
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                MovieGenre = movieGenre,
+                MovieRating = movieRating,
+                SortOption = (SortOptions)sortOption,
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Ratings = new SelectList(await ratingsQuery.Distinct().ToListAsync()),
+                Movies = await PaginatedList<Movie>.CreateAsync(movies.AsNoTracking(), pageNumber ?? 1, pageSize)
+            };
+
+
+            return View(movieGenreVM);
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movie.Remove(movie);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movie.Any(e => e.Id == id);
+        }
+    }
+}
