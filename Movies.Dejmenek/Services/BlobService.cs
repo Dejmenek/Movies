64f1947aaa990264e9ba8534e9568185ae396a17
Dejmenek <dejmen14567@gmail.com>
2024-12-26 20:12:45 +0100

    feat: add Azure Blob and Queue Storage client support

---
+++ Movies.Dejmenek/Services/BlobService.cs
@@ -0,0 +1,40 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace Movies.Dejmenek.Services;
+
+public class BlobService : IBlobService
+{
+    private readonly BlobServiceClient _blobServiceClient;
+    private const string _containerName = "files";
+    public BlobService(BlobServiceClient blobServiceClient)
+    {
+        _blobServiceClient = blobServiceClient;
+    }
+
+    public async Task DeleteAsync(string fileUri)
+    {
+        BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+
+        var fileName = Path.GetFileNameWithoutExtension(new Uri(fileUri).LocalPath);
+        BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+        await blobClient.DeleteIfExistsAsync();
+    }
+
+    public async Task<string> UploadAsync(IFormFile file)
+    {
+        BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+
+        var fileId = Guid.NewGuid();
+        BlobClient blobClient = containerClient.GetBlobClient(fileId.ToString());
+
+        using var stream = file.OpenReadStream();
+        await blobClient.UploadAsync(
+            stream,
+            new BlobHttpHeaders { ContentType = file.ContentType }
+        );
+
+        return blobClient.Uri.ToString();
+    }
+}
---
+++ Movies.Dejmenek/Services/IBlobService.cs
@@ -0,0 +1,7 @@
+﻿namespace Movies.Dejmenek.Services;
+
+public interface IBlobService
+{
+    Task<string> UploadAsync(IFormFile file);
+    Task DeleteAsync(string fileUri);
+}
