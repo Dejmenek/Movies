5f0739614ec87d5d0a5cd236ef17dd1ab0964b93
Dejmenek <dejmen14567@gmail.com>
2025-07-06 22:12:08 +0200

    refactor: use EditMovieViewModel for updating movie instead of MovieDTO
    Added the `EditMovieViewModel` clas. Updated the
    `Edit.cshtml` view to use this new model, replacing the
    previous `MovieDTO`. Introduced a conditional checkbox for
    removing the current image if it exists, enhancing user
    experience when editing movie details.

---
+++ Movies.Dejmenek/Models/EditMovieViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movies.Dejmenek.Models;
+
+public class EditMovieViewModel
+{
+    public int Id { get; set; }
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string? Title { get; set; }
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    [Required]
+    public DateTime? ReleaseDate { get; set; }
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
+    public string? Genre { get; set; }
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    [Required]
+    public decimal Price { get; set; }
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
+    public string? Rating { get; set; }
+    public IFormFile? ImageFile { get; set; }
+    public string? ImageUri { get; set; }
+    public bool RemoveImage { get; set; }
+}
